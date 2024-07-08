86abb5048a046f3c27ce5869db7308ad61b0df48
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-07-08 18:20:58 -0300

    functions

--- section01/main.go
+++ section01/main.go
@@ -6,10 +6,21 @@ import (
 
 func main() {
 
-	name := getName()
-	idade := 26
+	name, salario := "John", 100
+	setName(name)
+	newSalary, bonus := addSalary(salario, 10)
+	fmt.Println("Novo Salário:", newSalary)
+	fmt.Println("Bonus:", bonus)
+	getName()
 
-	fmt.Println("Hello", name, idade)
+}
+
+func setName(name string) {
+	fmt.Println("Hello", name)
+}
+
+func addSalary(valorAtual int, bonus int) (int, int) {
+	return valorAtual + bonus, bonus
 }
 
 func getName() string {
