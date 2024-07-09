e2d4fb411183aa67e2489ef740bbb768a5f23cfe
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-07-09 15:16:31 -0300

    array & slice

--- section01/main.go
+++ section01/main.go
@@ -12,14 +12,33 @@ type Pessoa struct {
 
 func main() {
 
-	pessoa := &Pessoa{
-		nome:    "John",
-		idade:   25,
-		salario: 100,
+	salarios := []int{}
+
+	// salarios := make([]int, 5)
+
+	for i := 0; i < 5; i++ {
+		salarios = append(salarios, 100+i)
 	}
 
-	addSalaryPessoa(pessoa, 10)
-	fmt.Println("Novo Salário:", pessoa.salario)
+	for _, salario := range salarios {
+		fmt.Println(salario)
+	}
+
+	// pessoa2 := new(Pessoa)
+	// pessoa2.nome = "John2"
+	// pessoa2.idade = 25
+	// pessoa2.salario = 100
+
+	// fmt.Println("Nome:", pessoa2.nome)
+
+	// pessoa := &Pessoa{
+	// 	nome:    "John",
+	// 	idade:   25,
+	// 	salario: 100,
+	// }
+
+	// pessoa.addSalaryPessoa(10)
+	// fmt.Println("Novo Salário:", pessoa.salario)
 
 	// name, salario := "John", 100
 	// setName(name)
@@ -38,7 +57,7 @@ func addSalary(valorAtual int, bonus int) (int, int) {
 	return valorAtual + bonus, bonus
 }
 
-func addSalaryPessoa(p *Pessoa, bonus int) {
+func (p *Pessoa) addSalaryPessoa(bonus int) {
 	p.salario += bonus
 }
 
